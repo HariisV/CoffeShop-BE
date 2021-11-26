953989f7298459770df1fcaa4f5803c1d3180ffc
Haris Wahyudi <hariswh9@gmail.com>
2021-11-27 00:36:29 +0700

    FIX Dashboard

--- src/modules/dashboard/dashboardController.js
+++ src/modules/dashboard/dashboardController.js
@@ -1,3 +1,4 @@
+const { filter } = require("compression");
 const helperWrapper = require("../../helper/wraper");
 const dashboardModel = require("./dashboardModel");
 
@@ -5,21 +6,98 @@ module.exports = {
   getDashboard: async (req, res) => {
     try {
       const { filter } = req.params;
-      if (filter === "WEEK" || filter === "DAY" || filter === "MONTH") {
-        const result = await dashboardModel.getDashboard(filter);
-        console.log(result[0].total);
+      // SELECT day(createdAt), SUM(totalPayment) As total FROM transaction_detail where WEEK(createdAt) = 47 GROUP BY DAY(createdAt);
+      if (filter === "WEEK") {
+        const allData = [];
+        let thisDate = new Date();
+        let curDate = new Date(
+          `${thisDate.getFullYear()}-${
+            thisDate.getMonth() + 1 > 10
+              ? thisDate.getMonth() + 1
+              : `0${thisDate.getMonth() + 1}`
+          }-01`
+        );
+        for (let i = 1; i <= 4; i++) {
+          let firstday = new Date(
+            curDate.setDate(curDate.getDate() - curDate.getDay())
+          );
+          let lastday = new Date(
+            curDate.setDate(curDate.getDate() - curDate.getDay() + 7)
+          );
+          firstday = `${firstday.getFullYear()}-${
+            firstday.getMonth() + 1 < 10
+              ? `0${firstday.getMonth() + 1}`
+              : firstday.getMonth() + 1
+          }-${
+            firstday.getDate() > 10
+              ? firstday.getDate()
+              : `0${firstday.getDate()}`
+          }`;
+
+          lastday = `${lastday.getFullYear()}-${
+            lastday.getMonth() + 1 < 10
+              ? `0${lastday.getMonth() + 1}`
+              : lastday.getMonth() + 1
+          }-${
+            lastday.getDate() > 10 ? lastday.getDate() : `0${lastday.getDate()}`
+          }`;
+
+          let result = await dashboardModel.getByWeek(firstday, lastday);
+          console.log(result);
+          allData.push({ name: `minggu-${i}`, total: result });
+        }
+        console.log(allData);
         return helperWrapper.response(
           res,
           200,
-          `Success Get Dashboard !`,
-          result[0].total
+          `Success Get Dashboard By Week !`,
+          allData
         );
-      } else {
+      } else if (filter === "MONTH") {
+        const allData = [];
+        const listMonth = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+        listMonth.forEach(async (element) => {
+          const result = await dashboardModel.getByMonth(element);
+          return allData.push({ name: element, total: result });
+        });
+        setTimeout(() => {
+          return helperWrapper.response(
+            res,
+            200,
+            `Success Get Dashboard By Month !`,
+            allData
+          );
+        }, 1000);
+      } else if (filter === "DAY") {
+        const date = new Date();
+        function daysInMonth(month, year) {
+          return new Date(year, month, 0).getDate();
+        }
+        const thisMonthDay = daysInMonth(date.getMonth(), date.getFullYear());
+        const listDay = [];
+
+        for (let i = 1; i <= thisMonthDay; i++) {
+          listDay.push(i);
+        }
+        const result = await dashboardModel.getByDay();
+
+        listDay.find(function (item, index) {
+          let ppp = result.find(({ DAY }) => DAY === item);
+          if (!ppp) {
+            result.push({
+              DAY: item,
+              total: 0,
+            });
+          }
+        });
+        result.sort(function (a, b) {
+          return a.DAY - b.DAY;
+        });
         return helperWrapper.response(
           res,
-          400,
-          `Wrong Filter, Only DAY | WEEK | MONTH`,
-          null
+          200,
+          `Success Get Dashboard By DAY !`,
+          result
         );
       }
     } catch (error) {
