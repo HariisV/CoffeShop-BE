ec61b0e2aac9ed459ff28b63577215c3ddd360fd
Haris Wahyudi <hariswh9@gmail.com>
2021-12-15 14:48:49 +0700

    make it better23

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,6 +1,7 @@
 const redis = require("redis");
 require("dotenv").config;
 
+console.log("HAIIIIII", process.env.RDS_hostname);
 const client = redis.createClient({
   host: process.env.RDS_hostname,
   port: process.env.RDS_port,
